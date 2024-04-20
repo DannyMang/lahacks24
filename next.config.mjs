73b77a17bae0e7fc17b7f57a28fbe8c5b6d6e0a3
daniel ung <daniel.ung@sjsu.edu>
2024-04-20 16:27:13 -0700

    fixed next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-    images: {
-      domains: ['via.placeholder.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
-=======
   async redirects() {
     return [
       {
@@ -21,4 +12,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
->>>>>>> e530acde83769a6b9718f7c2a0663b3186c62399
