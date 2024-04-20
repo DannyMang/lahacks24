c47f03b867785a97ca8c2ea80906770fd1f19f79
brandonnguyenn27 <brandonnguyenn27@gmail.com>
2024-04-20 02:26:58 -0700

    install shad, imagecomponent changes

--- src/components/gallery/ImageContainer.tsx
+++ src/components/gallery/ImageContainer.tsx
@@ -1,6 +1,17 @@
-"use client"
-import { Link } from '@nextui-org/react';
-import React from 'react';
+"use client";
+import { Button } from "@/components/ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { px } from "framer-motion";
+
+import React from "react";
 
 interface ImageProps {
   alt: string;
@@ -13,24 +24,43 @@ interface ImageProps {
   description: string;
 }
 
-const ImageComponent: React.FC<ImageProps> = ({ alt, className, height, src, style, width, title, description }) => {
+const ImageComponent: React.FC<ImageProps> = ({
+  alt,
+  className,
+  height,
+  src,
+  style,
+  width,
+  title,
+  description,
+}) => {
   return (
     <div className="relative group overflow-hidden rounded-lg">
-      <Link className="absolute inset-0 z-10" href="#">
-        <span className="sr-only">View</span>
-      </Link>
-      <img
-        alt={alt}
-        className={className}
-        height={height}
-        src={src}
-        style={style}
-        width={width}
-      />
-      <div className="absolute bottom-0 left-0 right-0 bg-gray-900 bg-opacity-50 text-white px-4 py-2 transition-all group-hover:translate-y-full">
-        <h3 className="text-lg font-semibold">{title}</h3>
-        <p className="text-sm">{description}</p>
-      </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="relative group overflow-hidden rounded-lg">
+            <img
+              alt={alt}
+              className={className}
+              height={height}
+              src={src}
+              style={style}
+              width={width}
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-gray-900 bg-opacity-50 text-white px-4 py-2 transition-all group-hover:translate-y-full">
+              <h3 className="text-lg font-semibold">{title}</h3>
+              <p className="text-sm">{description}</p>
+            </div>
+          </div>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle> {/* replace with post name */}
+            <img alt={alt} height={300} width={425} src={src} className="p-2" />
+            <p>{description}</p>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
