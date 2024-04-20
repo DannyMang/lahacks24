6b40d816f1f7d0f0287f81ab8ebd001dda371c60
brandonnguyenn27 <brandonnguyenn27@gmail.com>
2024-04-20 15:00:43 -0700

    added points to the globe

--- src/components/landingpage/GlobeComponent.tsx
+++ src/components/landingpage/GlobeComponent.tsx
@@ -3,25 +3,31 @@ import React, { useEffect, useRef } from "react";
 import Globe from "globe.gl";
 
 function GlobeComponent() {
-  type GlobeData = {
+  interface GlobeData {
     lat: number;
     lng: number;
     size: number;
     color: string;
-  };
+    label: string;
+  }
+
+  const plotPoints: GlobeData[] = [
+    {
+      lat: 34.0522,
+      lng: -118.2437,
+      size: 0.5,
+      color: "gold",
+      label: "Los Angeles",
+    },
+    { lat: 40.7128, lng: -74.006, size: 0.5, color: "red", label: "New York" },
+    { lat: 48.8566, lng: 2.3522, size: 0.5, color: "green", label: "Paris" },
+  ];
+
   const markerSvg = `<svg viewBox="-4 0 36 36">
     <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
     <circle fill="black" cx="14" cy="14" r="7"></circle>
   </svg>`;
   const globeEl = useRef<HTMLDivElement | null>(null);
-  const N = 30;
-  const gData = Array.from(Array(N).keys()).map(() => ({
-    lat: (Math.random() - 0.5) * 180,
-    lng: (Math.random() - 0.5) * 360,
-    size: 7 + Math.random() * 30,
-    color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-  }));
-
   useEffect(() => {
     // Initialize the globe
 
@@ -29,14 +35,10 @@ function GlobeComponent() {
     globe
       .globeImageUrl("//unpkg.com/three-globe/example/img/earth-day.jpg")
       .backgroundColor("rgba(0,0,0,0)") // Set to transparent or to a specific color matching your page
-      .htmlElementsData(gData)
-      .htmlElement((d) => {
-        const el = document.createElement("div");
-        el.innerHTML = markerSvg;
-        el.style.color = "red";
-        el.style.width = `${36}px`;
-        return el;
-      });
+      .pointsData(plotPoints)
+      .pointAltitude("size")
+      .pointColor("color")
+      .pointLabel("label");
 
     const controls = globe.controls();
     controls.enableZoom = false; // Disable zoom
